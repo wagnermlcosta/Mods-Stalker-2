a4db10eb922d616c402d90fadd2e00e0568862db
wagnermlcosta <wagnermlcosta@gmail.com>
2026-02-01 01:02:02 -0300

    chore: rename ObjPrototypes patch output to BetterSkif.

--- patch_ObjPrototypes.py
+++ patch_ObjPrototypes.py
@@ -7,7 +7,7 @@
 # - PRESERVAR indentação original
 
 INPUT_FILE = "ObjPrototypes.cfg"
-OUTPUT_FILE = "ObjPrototypes_patch_VaultSensitivity.cfg"
+OUTPUT_FILE = "ObjPrototypes_patch_BetterSkif.cfg"
 
 with open(INPUT_FILE, "r", encoding="utf-8") as f:
     lines = f.readlines()
